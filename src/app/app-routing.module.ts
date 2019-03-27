021b1c1e4edec5465cac4004ac7f6670205f9e88
Meir Donath <donathme@gmail.com>
2019-03-27 13:48:29 -0400

    Added NotFoundComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 const routes: Routes = [
   /*{path: 'about', component:AboutComponent},
-  {path: 'random', component:RandomDogComponent},
-  {path: '**', component:NotFoundComponent}*/
+  {path: 'random', component:RandomDogComponent},*/
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
